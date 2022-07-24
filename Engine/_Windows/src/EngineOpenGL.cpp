ee30deeea26068d3aad7c1bfe25e3125f7305287
ThePhoenix93 <francescofinotto@hotmail.com>
2022-07-24 03:10:27 +0200

    add debug glad files

--- Engine/_Windows/src/EngineOpenGL.cpp
+++ Engine/_Windows/src/EngineOpenGL.cpp
@@ -1,12 +1,16 @@
 #include <stdexcept>
 #include "EngineOpenGL.h"
+
 #include "glad/glad.h"
 namespace Platform::Win
 {
 
     void Initialize()
     {
-        if (!gladLoadGL())
+        if (int result = gladLoadGL(); result<=0)
+        {
+            auto errorCode = glGetError();
             throw std::runtime_error("Cannot initialize Glad");
+        }
     }
 }
